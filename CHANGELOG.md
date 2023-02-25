ea2405c3f0dd1f722c632f42826927fec96587d2
TormenTeDx <pipilek@gmail.com>
2023-02-25 18:30:43 +0100

    Add new version to detect palindromes on integers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # TormenTeDxPalindrome
 
-## Version 0.01
+## Version 0.1.0
+Added functionality to detect palindromes in strings (including case insensitive and with punctuation)
+
+## Version 0.1.1
+Added Palindrome detector to work on integers.
 
--- lib/TormenTeDx_palindrome.rb
+++ lib/TormenTeDx_palindrome.rb
@@ -2,16 +2,28 @@
 
 require_relative "TormenTeDx_palindrome/version"
 
-class String
+module TormenTeDxPalindrome
+
   # Returns true for a palindrome, false otherwise.
   def palindrome?
+    if processed_content.empty?
+      false
+    else
     processed_content == processed_content.reverse
+    end
   end
 
   private
   
   # Returns content for palindrome testing.
   def processed_content
-    scan(/[a-z]/i).join.downcase
+    to_s.scan(/[a-z0-9]/i).join.downcase
   end
+end
+
+class String
+  include TormenTeDxPalindrome
+end
+class Integer
+  include TormenTeDxPalindrome
 end
